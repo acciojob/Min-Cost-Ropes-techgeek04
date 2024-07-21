2048ddeaed2982f19e611af863a5055e69968dfa
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-21 22:26:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+ 
+  if (arr.length === 1) return 0;
+  arr.sort((a, b) => a - b);
+
+  let cost = 0;
+
+  while (arr.length > 1) {
+    const first = arr.shift();
+    const second = arr.shift();
+    const newRope = first + second;
+    cost += newRope;
+    let left = 0;
+    let right = arr.length;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (arr[mid] < newRope) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    arr.splice(left, 0, newRope);
+  }
+
+  return cost;
 }
 
-module.exports=mincost;
+// do not edit below this line
+module.exports = mincost;
